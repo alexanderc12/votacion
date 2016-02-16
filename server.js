eddc0e1a90278fe3d0fdb78dbdfbe1260836b63a
Alexander Castro Romero <alexanderc@outlook.com>
2016-02-16 08:42:54 -0500

    Update server.js

--- server.js
+++ server.js
@@ -61,9 +61,12 @@ app.use(function(err, req, res) {
     });
 });
 
+
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
 var server = http.createServer(app);
 
-server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function() {
-    var addr = server.address();
-    console.log("Server: ", addr.address + ":" + addr.port);
+server.listen(server_port, server_ip_address, function() {
+    console.log( "Listening on " + server_ip_address + ", server_port " + port );
 });
